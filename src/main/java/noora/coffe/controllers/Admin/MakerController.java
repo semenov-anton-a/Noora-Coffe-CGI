a51e7e8f8f1a777d8ed7d08838600f2d14e9d1c4
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-01 23:17:44 +0200

    up

--- src/main/java/noora/coffe/controllers/Admin/MakerController.java
+++ src/main/java/noora/coffe/controllers/Admin/MakerController.java
@@ -1,15 +1,18 @@
 package noora.coffe.controllers.Admin;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import noora.coffe.entity.*;
@@ -17,35 +20,104 @@ import noora.coffe.repos.*;
 import noora.coffe.services.*;
 
 @Controller
-public class MakerController extends CommonController{
-    
-    @Autowired
-    ProductService productService;
+public class MakerController extends CommonController {
 
+    @Autowired
+    DepartmentRepo departmentRepo;
+
+    @Autowired
+    ProductRepo productRepo;
+
+    @Autowired
+    DepartmentService departmentService;
+    
     @Autowired
     SupplierService supplierService;
 
+    @Autowired
+    ProductService productService;
 
-    @GetMapping("/admin/makers")
-    public String getDepartments(){
-        return "admin/makers";
+    // @Autowired
+    // // MakerService makerService;
+
+    /**
+     * 
+     * @param model
+     * @return
+     */
+    @ModelAttribute("suplierList")
+    private List<Supplier> getSupplierList(Model model, @RequestParam(defaultValue = "-1") Long Id) {
+        List<Supplier> sup = supplierService.getSupplierList(Id);
+        model.addAttribute("sup", sup);
+        model.addAttribute("styleActiveClass", Id);
+        return sup;
     }
-
-    @PostMapping(
-        path = "/admin/makers", 
-        consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
-    public String addNewSupplier( Supplier supplier, String option ){
-        switch ( option )
-        {
-            case "add": 
-                supplierService.addNewSupplier( supplier );      
-                break;
-            case "delete": 
-                
-                break;
-        }
-        return "redirect:/admin";
+    /**
+     * Show All Products
+     * @param model
+     * @param page
+     * @return
+     */
+    @GetMapping("/admin/suppliers")
+    public String getSuppliers(Model model, @RequestParam(defaultValue = "0") Integer page) {
+        this.getSupplierList(model, 0L);
+        Pageable pageable = PageRequest.of( page, this.itemsCoutOfPage );
+        Page<Product> productList = productService.getProduct_without_supplier( pageable );
+        
+        super.productPageable(model, productList, page, "suppliers");
+        model.addAttribute("productList", productList);
+        return "admin/suppliers";
     }
+    /**
+     * Show All Products
+     * @param model
+     * @param page
+     * @return
+     */
+    @GetMapping("/admin/suppliers/all")
+    public String getSupplierAll(Model model, @RequestParam(defaultValue = "0") Integer page) {
+        
+        Pageable pageable = PageRequest.of( page, this.itemsCoutOfPage );
+        Page<Product> productList = productService.getAll( pageable );
+        
+        super.productPageable(model, productList, page, "suppliers/all");
+        model.addAttribute("productList", productList);
+        return "admin/suppliers";
+    }
+    /**
+     * Show Products by Category
+     * @param model
+     * @return
+     */
+    @GetMapping("/admin/suppliers/{id}")
+    public String getDepartmentsById( Model model, @PathVariable Long id, @RequestParam(defaultValue = "0") Integer page ) {
+        this.getSupplierList(model, id);
 
+        Pageable pageable = PageRequest.of( page, this.itemsCoutOfPage );
+        Page<Product> productList = productRepo.findAllProductBySupplierId( id, pageable );
+        super.productPageable( model, productList, page,  "suppliers");
+        model.addAttribute("productList", productList);
+        return "admin/suppliers";
+    }
+    /**
+     * @POST (/admin/suppliers)
+     * @param department
+     * @return
+     */
+    @PostMapping( path = "/admin/suppliers",
+    consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE } ) 
+    public String addNewSupplier(
+            @RequestParam String name, 
+            @RequestParam String contact,  
+            @RequestParam String email,  
+            String option 
+    ){
 
-}
+        supplierService.addNewSupplier( new Supplier( name, contact, email, null ) );
+        return "redirect:/admin/suppliers";
+    }
+    // /**
+    //  * 
+    //  */
+
+}// END
